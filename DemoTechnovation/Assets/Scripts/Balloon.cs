039866c259786c08c3b35f6608dad3575a6f4d26
Antonio Mora <j_antonio_mora@hotmail.com>
2017-08-31 16:49:16 -0500

    Upload
    Demo Files

---
+++ DemoTechnovation/Assets/Scripts/Balloon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y > 7)
+        {
+            Destroy(this.gameObject);
+            Debug.Log(this.transform.position.y);
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ DemoTechnovation/Assets/Scripts/RespawnBalloon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnBalloon : MonoBehaviour {
+
+    public GameObject Balloon;
+    public float TimeToRespawn;
+    private const float y = -4.6f;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("StartGame", TimeToRespawn, TimeToRespawn);
+    }
+
+    private void StartGame()
+    {
+        float x = UnityEngine.Random.Range(-2.3f, 2.3f);
+        StartCoroutine(CreateBalloon());        
+        Instantiate(Balloon, new Vector3(x, y), Quaternion.identity);        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+
+    IEnumerator CreateBalloon(){
+        yield return new WaitForSeconds(10);
+    }
+}
